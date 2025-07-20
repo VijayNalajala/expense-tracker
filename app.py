19cf3e09a2cd445110902050e989cb840a95d13a
Vijay Nalajala <vijaynalajala99@gmail.com>
2025-07-20 11:10:30 +0530

    Updated app.py with dynamic port and host for deployment

--- app.py
+++ app.py
@@ -195,4 +195,7 @@ if __name__ == '__main__':
         with open('schema.sql') as f:
             conn.executescript(f.read())
         conn.close()
-    app.run(debug=True)
+    
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
